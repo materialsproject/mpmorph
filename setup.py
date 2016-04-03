440ce1a6f120315543f25ea5f8208110cfe23345
aykol <muratahan@gmail.com>
2016-04-02 23:02:51 -0700

    -

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='mpmorph',
+    version='0.1',
+    packages=['mpmorph', 'mpmorph.runners', 'mpmorph.analysis'],
+    url='',
+    license='MIT',
+    author='Muratahan Aykol',
+    author_email='maykol@lbl.gov',
+    description=''
+)
