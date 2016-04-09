c6d082fcf219524210d7e9eaca17cfff25de8713
aykol <muratahan@gmail.com>
2016-04-09 12:35:11 -0700

    cleanup

--- mpmorph/runners/amorphous_maker.py
+++ mpmorph/runners/amorphous_maker.py
@@ -2,23 +2,25 @@ from __future__ import division
 from pymatgen.io.vasp.inputs import Poscar
 from pymatgen.core.structure import Structure
 from collections import OrderedDict
-from pymatgen.io.vasp.sets import MITMDVaspInputSet
 import numpy as np
 import os
 
 
 class AmorphousMaker(object):
-    def __init__(self, el_num_dict, box_scale, tol = 2.0, packmol_path="packmol", clean=True):
+    def __init__(self, el_num_dict, box_scale, tol=2.0, packmol_path="packmol", clean=True):
         """
         Class for generating initial constrained-random packed structures for the
-        simulation of amorphous or liquid structures. This is a wrapper for "packmol".
+        simulation of amorphous or liquid structures. This is a wrapper for "packmol" package.
         Only works for cubic boxes for now.
-
         Args:
-        el_num_dict: dictionary of number of atoms of each species
-            e.g. {"V":22, "Li":10, "O":75, "B":10}
-        box_scale: all lattice vectors are multiplied with this scalar value
-            e.g. edge length of a cubic simulation box
+            el_num_dict (dict): dictionary of number of atoms of each species.
+                e.g. {"V":22, "Li":10, "O":75, "B":10}
+            box_scale (float): all lattice vectors are multiplied with this scalar value.
+                e.g. edge length of a cubic simulation box
+            tol (float): tolerance factor for how close the atoms can get (angstroms).
+                e.g. tol = 2.0 angstroms
+            packmol_path (str): path to the packmol executable
+            clean (bool): whether the intermedite files generated are deleted.
         """
         self.el_num_dict = el_num_dict
         self.box_scale = box_scale
@@ -30,7 +32,7 @@ class AmorphousMaker(object):
         self.clean = clean
 
     def __repr__(self):
-        return "AmorphousMaker: generates constrained-random packed initial structure for MD."
+        return "AmorphousMaker: generates constrained-random packed initial structure for MD using packmol."
 
     @property
     def box(self):
@@ -42,18 +44,18 @@ class AmorphousMaker(object):
     @property
     def random_packed_structure(self):
         """
-        Returns:
+        Returns: A constrained-random packed Structure object
         """
-        self._el_dict = self.call_packmol(clean=self.clean)
+        self._el_dict = self.call_packmol
         self._structure = self.get_structure(self._el_dict, self.box)
         return self._structure
 
-    def call_packmol(self, clean):
+    def call_packmol(self):
         """
-        Args:
-            clean: (bool) remove intermediate files during generation
         Returns:
-            dict of coordinates
+            A dict of coordinates of atoms for each element type
+            e.g. {'V': [[4.969925, 8.409291, 5.462153], [9.338829, 9.638388, 9.179811], ...]
+                  'Li': [[5.244308, 8.918049, 1.014577], [2.832759, 3.605796, 2.330589], ...]}
         """
 
         # this ensures periodic boundaries don't cause problems
@@ -68,18 +70,18 @@ class AmorphousMaker(object):
                           + "\nend structure\n\n")
         for el in self.el_num_dict.keys():
            with open(el+".xyz", "w") as f:
-               f.write("1\ncomment\n" + el +" 0.0 0.0 0.0\n")
+               f.write("1\ncomment\n" + el + " 0.0 0.0 0.0\n")
         try:
             os.system(self.packmol_path + " < packmol.input")
         except:
             raise OSError("packmol cannot be found!")
-        if clean:
-            for el in el_num_dict.keys():
+        if self.clean:
+            for el in self.el_num_dict.keys():
                 os.system("rm " + el + ".xyz")
             os.system("rm packmol.input")
-        return self.xyz2dict("mixture.xyz", clean=clean)
+        return self.xyz_to_dict("mixture.xyz")
 
-    def xyz2dict(self, filename, clean=False):
+    def xyz_to_dict(self, filename):
         """
         This is a generic xyz to dictionary convertor.
         Used to get the structure from packmol output.
@@ -97,7 +99,7 @@ class AmorphousMaker(object):
         if N != sum( [ len(x) for x in el_dict.values() ] ):
             raise ValueError("Inconsistent number of atoms")
         self._el_dict=OrderedDict(el_dict)
-        if clean:
+        if self.clean:
             os.system("rm " + filename)
         return self._el_dict
 
@@ -105,9 +107,10 @@ class AmorphousMaker(object):
     def get_structure(el_dict, lattice):
         """
         Args:
-            el_dict: is a dictionary of coordinates of each species
-                e.g. {'Cr': [ [0.0, 0.0, 0.0], [1.2, 1.2, 1.2], ...], 'O': [...] }
-            lattice: is the lattice in the form of [[x1,x2,x3],[y1,y2,y3],[z1,z2,z3]]
+            el_dict (dict): coordinates of atoms for each element type
+            e.g. {'V': [[4.969925, 8.409291, 5.462153], [9.338829, 9.638388, 9.179811], ...]
+                  'Li': [[5.244308, 8.918049, 1.014577], [2.832759, 3.605796, 2.330589], ...]}
+            lattice (list): is the lattice in the form of [[x1,x2,x3],[y1,y2,y3],[z1,z2,z3]]
         Returns: pymatgen Structure
         """
         species = []
@@ -121,15 +124,16 @@ class AmorphousMaker(object):
     def get_poscar(self):
         return Poscar(self.random_packed_structure)
 
-
     @staticmethod
     def xyzdict_to_poscar(el_dict, lattice, filepath ="POSCAR"):
         """
         Generates XYZ file from element coordinate dictionary and lattice
         Args:
-            el_dict: is a dictionary of coordinates of each species
-            lattice: is the lattice in the form of [[x1,x2,x3],[y1,y2,y3],[z1,z2,z3]]
-            filepath: path to POSCAR to be generated
+            el_dict (dict): coordinates of atoms for each element type
+            e.g. {'V': [[4.969925, 8.409291, 5.462153], [9.338829, 9.638388, 9.179811], ...]
+                  'Li': [[5.244308, 8.918049, 1.014577], [2.832759, 3.605796, 2.330589], ...]}
+            lattice (list): is the lattice in the form of [[x1,x2,x3],[y1,y2,y3],[z1,z2,z3]]
+            filepath (str): path to POSCAR to be generated
         Returns:
             writes a POSCAR file
         """
@@ -148,26 +152,3 @@ class AmorphousMaker(object):
             for key in el_dict.keys():
                 for atom in el_dict[key]:
                     f.write( " ".join( [ str(i) for i in atom ]) + "\n")
-
-
-if __name__ == "__main__":
-
-    # Example:
-    # LiBO2-V2O5 in a cubic box of edge 11.78 angstroms
-    a = 11.78
-    el_num_dict = {"V":22, "Li":10, "O":75, "B":10}
-
-    glass = AmorphousMaker(el_num_dict, a, packmol_path="~/packmol/packmol/packmol")
-    struct = glass.random_packed_structure
-
-
-    vaspmd = MITMDVaspInputSet(3000, 300, 2000, time_step=2, hubbard_off = False, spin_polarized = True,
-           user_incar_settings={'LWAVE':'.False.', 'LCHARG':'.True.', 'NCORE':24, 'EDIFF':1e-5,
-                                'ISIF':1, 'SMASS': 2, 'ENCUT': 520, 'SIGMA':0.1, 'ALGO':'Very Fast'},
-                                settings_file = "MPVaspInputSet.yaml")
-    vaspmd.write_input(struct, "./new_dir/")
-
-
-
-
-
