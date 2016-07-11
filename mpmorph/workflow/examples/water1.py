51309a90b3ae2b4847f4cad000f1c7a6b0ce5b28
aykol <muratahan@gmail.com>
2016-07-11 16:48:01 -0700

    one todo finished so removing it.

---
+++ mpmorph/workflow/examples/water1.py
@@ -0,0 +1,28 @@
+"""
+This example generates an AIMD workflow for finding equilibrium density for liquid water at 320 K.
+"""
+
+box_scale = 8.9 # edge length of MD box in Angstroms
+packmol_path = "~/packmol/packmol/packmol" # Revise as appropriate
+# "structure" in this context can be a dict of number of atoms.
+structure = {'H':40,'O':20}
+temperature = 320
+# Note one can use a pymatgen Structure object also
+# E.g. p = Poscar.from_file("POSCAR")
+#      structure = p.structure
+# MD runs can be backed up in a desired location in the
+# order they're run with copy_calcs option:
+copy_calcs = True
+calc_home = '~/test_H2O_wflows' # Revise as appropriate
+
+
+
+from mpmorph.workflow.workflows import get_wf_density
+from fireworks import LaunchPad
+amorphous_maker_params = {'box_scale':box_scale, 'packmol_path':packmol_path}
+name = 'H2O_df_'+str(temperature)
+wf = get_wf_density(structure, temperature=temperature, nsteps=1000, wall_time=19200, max_rescales=5,
+                    amorphous_maker_params=amorphous_maker_params, copy_calcs=copy_calcs, calc_home=calc_home, name=name)
+lp = LaunchPad.auto_load()
+lp.reset('', require_password=False)
+lp.add_wf(wf)
