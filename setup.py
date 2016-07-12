4e5d15b2cb7fcde735f29c497fc77f0583a91373
aykol <muratahan@gmail.com>
2016-07-11 22:14:56 -0700

    cleanup

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='mpmorph',
     version='0.1',
-    packages=['mpmorph', 'mpmorph.runners', 'mpmorph.analysis'],
-    url='',
-    license='MIT',
+    packages=find_packages(),
+    url='https://github.com/aykol/mpmorph',
+    license='modified BSD',
     author='Muratahan Aykol',
     author_email='maykol@lbl.gov',
-    description='', requires=['pymatgen', 'pymatgen']
+    description='',
+    install_requires=['matmethods>=1.2.7', 'fireworks>=1.2.7', 'scipy>=0.17.1', 'matplotlib>=1.6']
 )
