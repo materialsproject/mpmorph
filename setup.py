5e999d320057a8ef4d50b43198cde24247095914
aykol <muratahan@gmail.com>
2016-09-11 17:29:23 -0700

    cleanup

--- setup.py
+++ setup.py
@@ -8,6 +8,5 @@ setup(
     license='modified BSD',
     author='Muratahan Aykol',
     author_email='maykol@lbl.gov',
-    description='',
-    install_requires=['matmethods>=1.2.7', 'fireworks>=1.2.7', 'scipy>=0.17.1', 'matplotlib>=1.6']
+    description=''
 )
