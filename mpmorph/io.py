08b4340b53b65cae088c204ee80177c7570c1f44
Eric <sivonxay@gmail.com>
2017-08-21 11:14:10 -0700

    Added XDATCAR writer

--- mpmorph/io.py
+++ mpmorph/io.py
@@ -3,12 +3,12 @@ from monty.io import zopen
 
 class Xdatcar_Structures(Xdatcar):
 
-    def write_xdatcar(self, filename="XDATCAR", **kwargs):
+    def write_xdatcar(self, **kwargs):
         """
         Writes Xdatcar to a file. The supported kwargs are the same as those for
         the Xdatcar_from_structs.get_string method and are passed through directly.
         """
-        with zopen(filename, "wt") as f:
+        with zopen(self.filename, "wt") as f:
             f.write(self.get_string_from_struct(**kwargs))
 
     def get_string_from_struct(self, structures, system="unknown system", significant_figures=6):
