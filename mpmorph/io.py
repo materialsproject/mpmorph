cd4e847d58da364f7c59e3c9d991b583979c3a21
Eric <sivonxay@gmail.com>
2017-08-21 11:26:26 -0700

    Added XDATCAR writer

--- mpmorph/io.py
+++ mpmorph/io.py
@@ -1,7 +1,7 @@
-from pymatgen.io.vasp.outputs import Xdatcar
 from monty.io import zopen
+import itertools
 
-class Xdatcar_Structures(Xdatcar):
+class Xdatcar_Writer():
 
     def write_xdatcar(self, filename, **kwargs):
         """
@@ -13,8 +13,8 @@ class Xdatcar_Structures(Xdatcar):
 
     def get_string_from_struct(self, structures, system="unknown system", significant_figures=6):
         lines = [system, "1.0", str(structures[0].lattice)]
-        lines.append(" ".join(self.site_symbols))
-        lines.append(" ".join([str(x) for x in self.natoms]))
+        lines.append(" ".join(self.get_site_symbols(structures[0])))
+        lines.append(" ".join([str(x) for x in self.get_natoms(structures[0])]))
 
         format_str = "{{:.{0}f}}".format(significant_figures)
         for (si, structure) in enumerate(structures):
@@ -26,3 +26,19 @@ class Xdatcar_Structures(Xdatcar):
                 lines.append(line)
 
         return "\n".join(lines) + "\n"
+
+    def get_site_symbols(self, structure):
+        """
+        Sequence of symbols associated with the Poscar. Similar to 6th line in
+        vasp 5+ POSCAR.
+        """
+        syms = [site.specie.symbol for site in structure]
+        return [a[0] for a in itertools.groupby(syms)]
+
+    def get_natoms(self, structure):
+        """
+        Sequence of number of sites of each type associated with the Poscar.
+        Similar to 7th line in vasp 5+ POSCAR or the 6th line in vasp 4 POSCAR.
+        """
+        syms = [site.specie.symbol for site in structure]
+        return [len(tuple(a[1])) for a in itertools.groupby(syms)]
