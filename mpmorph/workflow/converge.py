4a49c9df5cba0ec035eb3213fc90ee240c7d5601
sivonxay <sivonxay@gmail.com>
2017-09-25 11:35:22 -0700

    Added Production Run Labels

--- mpmorph/workflow/converge.py
+++ mpmorph/workflow/converge.py
@@ -49,7 +49,7 @@ def get_converge(structure, priority = None, preconverged=False, prod_quants={"n
     while prod_steps <= prod_quants["target"] - prod_quants["nsteps"]:
         run_args = {"md_params": {"start_temp": run_args["md_params"]["end_temp"], "end_temp": run_args["md_params"]["end_temp"], "nsteps":5000},
                     "run_specs":{"vasp_input_set": None ,"vasp_cmd": ">>vasp_cmd<<", "db_file": ">>db_file<<", "wall_time": 86400},
-                    "optional_fw_params":{"override_default_vasp_params":{}, "copy_vasp_outputs": False, "spec":{}}
+                    "optional_fw_params":{"override_default_vasp_params":{}, "copy_vasp_outputs": False, "spec":{}},
                     "label": "prod_run_"}
 
         run_args["optional_fw_params"]["override_default_vasp_params"].update(
