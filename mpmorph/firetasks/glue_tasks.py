52e6bfe14bfe175a758e455c2e16628e76ebec89
jcheng <jic198@ucsd.edu>
2018-09-06 15:54:36 -0700

    msg

--- mpmorph/firetasks/glue_tasks.py
+++ mpmorph/firetasks/glue_tasks.py
@@ -1,7 +1,8 @@
+import os
+import numpy as np
 from fireworks import explicit_serialize, FireTaskBase, FWAction
 from pymatgen.io.vasp import Poscar
-from pymatgen import Structure
-import os
+from pymatgen import Structure, Lattice
 
 
 @explicit_serialize
@@ -34,11 +35,17 @@ class SaveStructureTask(FireTaskBase):
 
         if self.get("rescale_volume", False):
             spec_structure = Structure.from_dict(fw_spec["structure"])
-            scaling_volume = _poscar.structure.volume
-            spec_structure.scale_lattice(scaling_volume)
-            _structure = spec_structure.as_dict()
+            for isite, site in enumerate(spec_structure):
+                spec_structure[isite] = site.to_unit_cell
+            new_lattice = Lattice(_poscar.structure.volume ** (1 / 3.0) * np.eye(3))
+            new_fract = new_lattice.get_fractional_coords(spec_structure.cart_coords)
+            new_s = Structure(new_lattice, spec_structure.species, new_fract,
+                              charge=spec_structure.charge,
+                              site_properties=spec_structure.site_properties)
+            _structure = new_s.as_dict()
 
         with open("sst_out", 'w') as f:
             f.write(str(_structure))
             f.close()
         return FWAction(update_spec={"structure": _structure})
+
