e16748596959a91d5acce88cf31be35248703270
jcheng <jic198@ucsd.edu>
2018-09-17 09:26:39 -0700

    Message

--- mpmorph/workflow/quench.py
+++ mpmorph/workflow/quench.py
@@ -7,8 +7,8 @@ from mpmorph.util import recursive_update
 import numpy as np
 
 
-def get_quench(structures, temperatures={}, priority=None, quench_type="simulated_anneal", cool_args={}, hold_args={}, quench_args={},
-               descriptor="", **kwargs):
+def get_quench(structures, temperatures={}, priority=None, quench_type="simulated_anneal",
+               cool_args={}, hold_args={}, quench_args={}, descriptor="", **kwargs):
     fw_list = []
     if temperatures == {}:
         temperatures = {"start_temp": 3000, "end_temp": 500, "temp_step": 500}
@@ -28,7 +28,8 @@ def get_quench(structures, temperatures={}, priority=None, quench_type="simulate
                 _fw = get_MDFW(structure, temp, temp - temperatures["temp_step"],
                                name="snap_" + str(i) + "_cool_" + str(temp - temperatures["temp_step"]),
                                args=cool_args, parents=[_fw_list[-1]] if len(_fw_list) > 0 else [],
-                               priority=priority, previous_structure=use_prev_structure, insert_db=False, **kwargs)
+                               priority=priority, previous_structure=use_prev_structure,
+                               insert_db=False, **kwargs)
                 _fw_list.append(_fw)
                 # get fw for hold step
                 _fw = get_MDFW(structure, temp - temperatures["temp_step"], temp - temperatures["temp_step"],
@@ -39,19 +40,26 @@ def get_quench(structures, temperatures={}, priority=None, quench_type="simulate
 
         if quench_type in ["simulated_anneal", "mp_quench"]:
             # Relax OptimizeFW and StaticFW
-            run_args = {"run_specs": {"vasp_input_set": None, "vasp_cmd": ">>vasp_cmd<<", "db_file": ">>db_file<<",
-                                      "spec": {"_priority": priority}},
-                        "optional_fw_params": {"override_default_vasp_params": {}}}
+            run_args = {"run_specs": {"vasp_input_set": None, "vasp_cmd": ">>vasp_cmd<<",
+                                      "db_file": ">>db_file<<",
+                                      "spec": {"_priority": priority}
+                                      },
+                        "optional_fw_params": {"override_default_vasp_params": {}}
+                        }
             run_args = recursive_update(run_args, quench_args)
-            _name = str(structure.composition.reduced_formula) + "_snap_" + str(i)
+            _name = "_snap_" + str(i)
 
             fw1 = OptimizeFW(structure=structure, name=_name + descriptor + "_optimize",
-                             parents=[_fw_list[-1]] if len(_fw_list) > 0 else [], **run_args["run_specs"], **run_args["optional_fw_params"], max_force_threshold=None)
+                             parents=[_fw_list[-1]] if len(_fw_list) > 0 else [],
+                             **run_args["run_specs"], **run_args["optional_fw_params"],
+                             max_force_threshold=None)
             if len(_fw_list) > 0:
                 fw1 = powerups.add_cont_structure(fw1)
             fw1 = powerups.add_pass_structure(fw1)
 
-            fw2 = StaticFW(structure=structure, name=_name + descriptor + "_static", parents=[fw1], **run_args["run_specs"], **run_args["optional_fw_params"])
+            fw2 = StaticFW(structure=structure, name=_name + descriptor + "_static",
+                           parents=[fw1], **run_args["run_specs"],
+                           **run_args["optional_fw_params"])
             fw2 = powerups.add_cont_structure(fw2)
             fw2 = powerups.add_pass_structure(fw2)
 
@@ -64,11 +72,16 @@ def get_quench(structures, temperatures={}, priority=None, quench_type="simulate
     return wf
 
 
-def get_MDFW(structure, start_temp, end_temp, name="molecular dynamics", priority=None, job_time=None, args={}, **kwargs):
+def get_MDFW(structure, start_temp, end_temp, name="molecular dynamics", priority=None,
+             job_time=None, args={}, **kwargs):
     run_args = {"md_params": {"nsteps": 500},
-                "run_specs": {"vasp_input_set": None, "vasp_cmd": ">>vasp_cmd<<", "db_file": ">>db_file<<",
-                              "wall_time": 40000},
-                "optional_fw_params": {"override_default_vasp_params": {}, "copy_vasp_outputs": False, "spec": {}}}
+                "run_specs": {"vasp_input_set": None, "vasp_cmd": ">>vasp_cmd<<",
+                              "db_file": ">>db_file<<", "wall_time": 40000
+                              },
+                "optional_fw_params": {"override_default_vasp_params": {},
+                                       "copy_vasp_outputs": False, "spec": {}
+                                       }
+                }
 
     run_args["optional_fw_params"]["override_default_vasp_params"].update(
         {'user_incar_settings': {'ISIF': 1, 'LWAVE': False}})
@@ -77,6 +90,6 @@ def get_MDFW(structure, start_temp, end_temp, name="molecular dynamics", priorit
     run_args["md_params"]["end_temp"] = end_temp
     run_args["optional_fw_params"]["spec"]["_priority"] = priority
     run_args["optional_fw_params"]["spec"]["_queueadapter"] = {"walltime": job_time}
-    _mdfw = MDFW(structure=structure, name=name, **run_args["md_params"], **run_args["run_specs"],
-                 **run_args["optional_fw_params"], **kwargs)
+    _mdfw = MDFW(structure=structure, name=name, **run_args["md_params"],
+                 **run_args["run_specs"], **run_args["optional_fw_params"], **kwargs)
     return _mdfw
