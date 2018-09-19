2e40af73be694abb82bc184985b95ac09f058071
Unknown <sivonxay@gmail.com>
2018-09-19 16:22:31 -0700

    Added vasp_opt to new pressure convergence.

--- mpmorph/workflow/converge.py
+++ mpmorph/workflow/converge.py
@@ -108,9 +108,8 @@ def get_converge(structure, priority=None, preconverged=False, max_steps=5000, t
     return wf
 
 
-def get_converge_by_fit(structure, temperature, images=[0.8, 1, 1.2], preconverged=False, max_steps=5000,
-                        target_steps=40000, spawner_args={}, converge_args={}, prod_args={},
-                        converge_type=[("density", 5)], **kwargs):
+def get_converge_new(structure, temperature, converge_scheme='EOS', preconverged=False, max_steps=5000,
+                     target_steps=10000, priority=None, **kwargs):
     """
 
     :param structure: Starting structure for the run
@@ -119,11 +118,9 @@ def get_converge_by_fit(structure, temperature, images=[0.8, 1, 1.2], preconverg
     :param preconverged: Is the structure already converged (i.e. Pressure 0bar) or volume rescaling not desired?
     :param max_steps: Maximum number of steps per chunk of production run MD simulation
     :param target_steps: Target number of steps for production MD run
-    :param spawner_args:
-    :param converge_args:
-    :param prod_args:
-    :param converge_type: Type of convergence can specify ("density", <tolerance value>) or ("ionic", <tolerance value>)
-    :param kwargs:
+    :param convergence_criteria: Type of convergence can specify ("density", <tolerance value>) or ("ionic", <tolerance value>)
+    :param priority: Priority of all fireworks in the workflow
+    :param kwargs: arguments such as spawner_args, converge_args, convergence_criteria, etc.
     :return:
     """
 
@@ -137,45 +134,80 @@ def get_converge_by_fit(structure, temperature, images=[0.8, 1, 1.2], preconverg
                     "override_default_vasp_params": {'user_incar_settings': {'ISIF': 1, 'LWAVE': False}},
                     "copy_vasp_outputs": False, "spec": {}}}
     run_args["optional_fw_params"]["spec"]["_queueadapter"] = {"walltime": run_args["run_specs"]["wall_time"]}
-    run_args = recursive_update(run_args, converge_args)
+    run_args = recursive_update(run_args, kwargs.get('converge_args', {}))
+    run_args["optional_fw_params"]["spec"]["_priority"] = priority
+
+    # Setup Dictionary specifying parameters of the spawner for convergence tasks
+    _spawner_args = {
+        "converge_params": {"max_rescales": 15, "density_spawn_count": 1, "energy_spawn_count": 0},
+        "rescale_params": {"beta": 0.0000005},
+        "run_specs": run_args["run_specs"], "md_params": run_args["md_params"],
+        "optional_fw_params": run_args["optional_fw_params"]}
+    _spawner_args['converge_params']['converge_type'] = kwargs.get('convergence_criteria',
+                                                                   [("density", 5), ('ionic', '0.001')])
+    _spawner_args["md_params"].update({"start_temp": run_args["md_params"]["end_temp"]})
+    _spawner_args = recursive_update(_spawner_args, kwargs.get('spawner_args', {}))
+    _spawner_args["optional_fw_params"]["spec"]["_priority"] = priority
 
     # Converge the pressure (volume) of the system
     if not preconverged:
-        # Create structures for varying volumes
-        structures = [structure.copy() for i in images]
-        for i, factor in enumerate(images):
-            structures[i].scale_lattice(structure.volume * factor)
+        if converge_scheme == 'EOS':
+            # Create structures for varying volumes
+            images = kwargs.get('images', [0.8, 1, 1.2])
+            structures = [structure.copy() for i in images]
+            for i, factor in enumerate(images):
+                structures[i].scale_lattice(structure.volume * factor)
 
-        # Create firework for each structure
-        volume_fws = []
-        for i, vol_structure in zip(images, structures):
-            _fw = MDFW(structure=vol_structure, name="volume_" + str(i), previous_structure=False, insert_db=False,
+            # Create firework for each structure
+            volume_fws = []
+            for i, vol_structure in zip(images, structures):
+                _fw = MDFW(structure=vol_structure, name="volume_" + str(i), previous_structure=False, insert_db=False,
+                           **run_args["md_params"], **run_args["run_specs"],
+                           **run_args["optional_fw_params"])
+
+                _fw = powerups.add_pass_pv(_fw)
+                volume_fws.append(_fw)
+            fw_list.extend(volume_fws)
+
+            # Create firework to converge pressure/volume
+            _spawner_args['rescale_params']['beta'] = 0.000001
+            spawner_fw = MDFW(structure=structure, name="run1", previous_structure=True, insert_db=False,
+                              parents=volume_fws,
+                              **run_args["md_params"], **run_args["run_specs"], **run_args["optional_fw_params"])
+
+            spawner_fw = powerups.add_PV_volume_rescale(spawner_fw)
+            spawner_fw = powerups.add_pass_pv(spawner_fw)
+            spawner_fw = powerups.add_converge_task(spawner_fw, **_spawner_args)
+            fw_list.append(spawner_fw)
+        elif converge_scheme == 'vasp_opt':
+            fw1 = MDFW(structure=structure, name="run0", previous_structure=False, insert_db=False,
                        **run_args["md_params"], **run_args["run_specs"],
                        **run_args["optional_fw_params"])
 
-            _fw = powerups.add_pass_pv(_fw)
-            volume_fws.append(_fw)
-        fw_list.extend(volume_fws)
+            #Create Vasp Optimization firework
+            optimize_args = {"run_specs": {"vasp_input_set": None, "vasp_cmd": ">>vasp_cmd<<", "db_file": ">>db_file<<",
+                                           "spec": {"_priority": priority}}}
+            fw2 = OptimizeFW(structure=structure, name="rescale_optimize", insert_db=False, job_type="normal",
+                             parents=[fw1], **optimize_args["run_specs"], max_force_threshold=None)
+            fw2 = powerups.add_cont_structure(fw2)
+            fw2 = powerups.replace_pass_structure(fw2, rescale_volume=True)
 
-        # Create Dictionary specifying parameters of spawner
-        _spawner_args = {
-            "converge_params": {"converge_type": converge_type, "max_rescales": 15, "density_spawn_count": 1,
-                                "energy_spawn_count": 0},
-            "rescale_params": {"beta": 0.0000005},
-            "run_specs": run_args["run_specs"], "md_params": run_args["md_params"],
-            "optional_fw_params": run_args["optional_fw_params"]}
-        _spawner_args["md_params"].update({"start_temp": run_args["md_params"]["end_temp"]})
-        _spawner_args = recursive_update(_spawner_args, spawner_args)
+            # Create Convergence Firework
+            if len(_spawner_args["md_params"].keys()) > 0:
+                run_args["md_params"].update(_spawner_args["md_params"])
+            fw3 = MDFW(structure=structure, name="run1", previous_structure=True, insert_db=False,
+                       **run_args["md_params"],
+                       parents=[fw2], **run_args["run_specs"], **run_args["optional_fw_params"])
 
-        # Create firework to converge pressure/volume
-        spawner_fw = MDFW(structure=structure, name="run1", previous_structure=True, insert_db=False,
-                          parents=volume_fws,
-                          **run_args["md_params"], **run_args["run_specs"], **run_args["optional_fw_params"])
+            fw3 = powerups.add_converge_task(fw3, **_spawner_args)
 
-        spawner_fw = powerups.add_PV_volume_rescale(spawner_fw)
-        spawner_fw = powerups.add_pass_pv(spawner_fw)
-        spawner_fw = powerups.add_converge_task(spawner_fw, **_spawner_args)
-        fw_list.append(spawner_fw)
+            fw_list.extend([fw1, fw2, fw3])
+        else:
+            fw1 = MDFW(structure=structure, name="run0", previous_structure=False, insert_db=False,
+                       **run_args["md_params"], **run_args["run_specs"],
+                       **run_args["optional_fw_params"])
+            fw1 = powerups.add_converge_task(fw1, **_spawner_args)
+            fw_list.append(fw1)
 
     # Production length MD runs
     prod_steps = 0
@@ -191,7 +223,8 @@ def get_converge_by_fit(structure, temperature, images=[0.8, 1, 1.2], preconverg
 
         run_args["optional_fw_params"]["override_default_vasp_params"].update(
             {'user_incar_settings': {'ISIF': 1, 'LWAVE': False}})
-        run_args = recursive_update(run_args, prod_args)
+        run_args = recursive_update(run_args, kwargs.get('prod_args', {}))
+        run_args["optional_fw_params"]["spec"]["_priority"] = priority
         parents = fw_list[-1] if len(fw_list) > 0 else []
         previous_structure = False if preconverged and i == 0 else True
         fw = MDFW(structure=structure, name=run_args["label"] + str(i) + "-" + str(tag_id),
