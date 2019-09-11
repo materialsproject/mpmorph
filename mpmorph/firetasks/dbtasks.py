4e1087d307abed1687acf6be8e8c7b15c3cd86d3
Unknown <sivonxay@gmail.com>
2019-09-11 13:22:06 -0700

    Fix regex for task matching

--- mpmorph/firetasks/dbtasks.py
+++ mpmorph/firetasks/dbtasks.py
@@ -114,7 +114,8 @@ class TrajectoryDBTask(FiretaskBase):
         mmdb.db.trajectories.find_one_and_delete({"runs_label": tag_id})
         runs = mmdb.db['tasks'].find(
             {"task_label": re.compile(f'.*{tag_id}.*')})
-        runs_sorted = sorted(runs, key=lambda x: int(re.findall('run-(\d+)', x['task_label'])[0]))
+
+        runs_sorted = sorted(runs, key=lambda x: int(re.findall('run[_-](\d+)', x['task_label'])[0]))
 
         trajectory_doc = runs_to_trajectory_doc(runs_sorted, db_file, tag_id, notes)
 
