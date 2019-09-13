f4486194bc6a38224b81ee061660f9ac68d54246
Unknown <sivonxay@gmail.com>
2019-09-13 14:51:46 -0700

    Update package to 2.0

--- mpmorph/database/__init__.py
+++
@@ -1,1 +0,0 @@
-__author__ = 'Eric Sivonxay <esivonxay@lbl.gov>'
--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='mpmorph',
-    version='1.0',
+    version='2.0',
     packages=find_packages(),
-    url='https://github.com/aykol/mpmorph',
+    url='https://github.com/materialsproject/mpmorph',
     license='modified BSD',
     author='Muratahan Aykol',
     author_email='maykol@lbl.gov'
