88fbcf5cf008015afaae049ca436cfc614cddac1
Unknown <sivonxay@gmail.com>
2019-09-16 16:59:06 -0700

    Move rescaling to from previous structure task

--- mpmorph/firetasks/glue_tasks.py
+++ mpmorph/firetasks/glue_tasks.py
@@ -12,10 +12,18 @@ __author__ = 'Eric Sivonxay <esivonxay@lbl.gov>'
 @explicit_serialize
 class PreviousStructureTask(FireTaskBase):
     required_params = []
-    optional_params = []
+    optional_params = ["rescale_volume"]
 
     def run_task(self, fw_spec):
         structure_dict = fw_spec["structure"]
+
+        if self.get("rescale_volume", False):
+            spec_structure = Structure.from_dict(structure_dict)
+            scaling_volume = spec_structure.volume * self["rescale_volume"]
+
+            spec_structure.scale_lattice(scaling_volume)
+            structure_dict = spec_structure.as_dict()
+
         _poscar = Poscar(structure_dict)
         _poscar.write_file("POSCAR")
         return FWAction()
@@ -24,30 +32,19 @@ class PreviousStructureTask(FireTaskBase):
 @explicit_serialize
 class SaveStructureTask(FireTaskBase):
     required_params = []
-    optional_params = ["rescale_volume"]
+    optional_params = []
 
     def run_task(self, fw_spec):
         osw = list(os.walk("."))[0]
         files = []
 
-        contcar_exists = False
         for file_name in osw[2]:
             if "CONTCAR" in file_name:
                 files.append(file_name)
-                contcar_exists = True
 
         _poscar = Poscar.from_file(filename=files[-1], check_for_POTCAR=True, read_velocities=True)
         _structure = _poscar.structure.as_dict()
 
-        if self.get("rescale_volume", False):
-            spec_structure = Structure.from_dict(fw_spec["structure"])
-            scaling_volume = _poscar.structure.volume
-            spec_structure.scale_lattice(scaling_volume)
-            _structure = spec_structure.as_dict()
-
-        with open("sst_out", 'w') as f:
-            f.write(str(_structure))
-            f.close()
         return FWAction(update_spec={"structure": _structure})
 
 
