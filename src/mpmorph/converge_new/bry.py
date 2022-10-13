9895ba7f368eb7c20da2ceef2b2c665e55649c62
Bryant <bryantli@lbl.gov>
2022-10-13 15:35:53 -0700

    converge flow works in bry.py

--- src/mpmorph/converge_new/bry.py
+++ src/mpmorph/converge_new/bry.py
@@ -1,7 +1,9 @@
 from distutils.log import ERROR
+from doctest import master
 from operator import eq
-from atomate2.vasp.jobs.core import MDMaker, MDSetGenerator
+from atomate2.vasp.jobs.core import MDMaker
 from jobflow import Flow, job, Response
+import numpy as np
 
 from mpmorph.runners import rescale_volume
 
@@ -13,10 +15,11 @@ def get_converge_flow(structure):
     md_jobs = [get_md_job(structure,factor) for factor in scale_factors]
 
     #Dynamic job
-    PV_rescale_job = PV_rescale(structure, [job.outputs for job in md_jobs])
-    production_run_md_job = MDMaker(name = 'unique identifier_struc_scaler').make(PV_rescale_job.output)
-    
-    flow = Flow([md_jobs, PV_rescale_job, production_run_md_job])
+    PV_rescale_job = PV_rescale(structure, [job.output for job in md_jobs])
+    production_run_md_job = MDMaker().make(PV_rescale_job.output)
+    production_run_md_job.maker.input_set_generator.user_incar_settings["NSW"] = NSW  #runs it for short timestep (designed for debugging)
+
+    flow = Flow([*md_jobs, PV_rescale_job, production_run_md_job])
     
     return flow 
 
@@ -28,15 +31,14 @@ def PV_rescale(structure, md_jobs_outputs):
 
     final_structure = structure.copy()
 
-    pv_pairs = np.array([job['pressure_volume'] for job in md_jobs_outputs]) #(Obtained from MPMorph) it should be tuples
-    pv_pairs = np.flip(pv_pairs, axis=1) 
-    pv_pairs = np.flip(pv_pairs[pv_pairs[:, 1].argsort()], axis=0)
+    volume_data = [task_doc_to_volume_data(doc) for doc in md_jobs_outputs]
+    pressure_data = [task_doc_to_pressure_data(doc) for doc in md_jobs_outputs]
 
-    max_vol_scale = 
-    min_vol_scale =
+    max_vol_scale = max(volume_data)
+    min_vol_scale = min(volume_data)
 
 
-    params = fit_BirchMurnaghanPV_EOS(pv_pairs)
+    params = rescale_volume.fit_BirchMurnaghanPV_EOS(zip(volume_data,pressure_data))
     equil_volume = params[0]
     
     if equil_volume < max_vol_scale and equil_volume > min_vol_scale:
@@ -57,8 +59,7 @@ def PV_rescale(structure, md_jobs_outputs):
 
     return Response(replace = flow)
 
-MAX_MD_JOBS = 5 #if you can't converge with five additional calcs you're doing something wrong...
-OFFSET = 0.1 #gives it enough room to slosh back
+
 def get_new_max_volume(equil_guess, original_structure):
     return equil_guess/original_structure.volume + OFFSET
 
@@ -66,20 +67,25 @@ def get_new_min_volume(equil_guess, original_structure):
     return equil_guess/original_structure.volume - OFFSET
 
 def task_doc_to_volume_data(task_doc): #TODO
-    volume = task_doc['output']['output']['stress']
+    volume = task_doc.calcs_reversed[-1].output.ionic_steps[-1].structure.lattice.volume
+    return volume
 
 def task_doc_to_pressure_data(task_doc): #TODO
-    stress_tensor = task_doc['output']['output']['stress']
-    pressure = trace(stress_tensor)
+    stress_tensor = task_doc.calcs_reversed[-1].output.ionic_steps[-1].stress
+    pressure = 1/3 * np.trace(stress_tensor)
     return pressure
 
 def get_md_job(structure, factor):
     struct = structure.copy()
     structure.scale_lattice(struct.volume * factor)
     md_job= MDMaker().make(struct)
-    md_job.maker.input_set_generator.user_incar_settings["NSW"] = 10  
+    md_job.maker.input_set_generator.user_incar_settings["NSW"] = NSW  #runs it for short timestep (designed for debugging)
     md_job.metadata.update({"scale_factor": factor})
-    md_job.StorePressureVolumeDatainTaskDoc #
 
     return md_job
 
+
+#Params list
+MAX_MD_JOBS = 5 #if you can't converge with five additional calcs you're doing something wrong...
+OFFSET = 0.1 #gives it enough room to slosh back
+NSW = 10
