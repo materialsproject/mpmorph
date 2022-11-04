243812c4df6a4fbef5f4f9b62518954f052b2399
Max Gallant <mcgallant72@gmail.com>
2022-11-04 16:22:26 -0700

    switch to simple dataclass

--- src/mpmorph/flows/md_flow.py
+++ src/mpmorph/flows/md_flow.py
@@ -20,6 +20,7 @@ def get_converge_flow_m3gnet(structure, temp, steps: int = 1000):
         temperature=temp,
         steps=steps
     )
+    print(inputs)
     pv_md_maker = PVFromM3GNet(parameters=inputs)
     return get_converge_flow(structure, pv_md_maker)
 
--- src/mpmorph/jobs/tasks/m3gnet_input.py
+++ src/mpmorph/jobs/tasks/m3gnet_input.py
@@ -1,69 +1,26 @@
 from typing import Optional
 
-from pydantic import BaseModel, Field
+from dataclasses import dataclass
+
 from ase import units
 
 def one_atmosphere():
     return 1.01325 * units.bar
 
-class M3GNetMDInputs(BaseModel):
+@dataclass
+class M3GNetMDInputs():
 
-    ensemble: str = Field("nvt", description="The ensemble to use during the MD run.")
-
-    temperature: float = Field(2000.0, description="The temperature of the simulation")
-
-    pressure: float = Field(
-        default_factory=one_atmosphere,
-        description="The pressure at which this simulation should be run."
-    )
-
-    timestep: float = Field(
-        1.0, 
-        description="The timestep for this simulation in femtoseconds"
-    )
-
-    potential: Optional[str] = Field(
-        None,
-        description="The potential for use in this simulation"
-    )
-    taut: Optional[float] = Field(
-        None, 
-        description="The time constant for Berendsen temperature coupling"
-    )
-    taup: Optional[float] = Field(
-        None,
-        description="The time constant for pressure coupling"
-    )
-    compressibility_au: Optional[float] = Field(
-        None,
-        description="The compressibility of the material in A^3/eV"
-    )
-    trajectory_fn: str = Field(
-        "out.traj", 
-        description="The name of file which should be used to store the trajectory"
-    )
-    logfile_fn: str = Field(
-        "out.log", 
-        description="The name of the file for the simulation log"
-    )
-    loginterval: int = Field(
-        1,
-        description="Write to the logfile every {this number} steps"
-    )
-    append_trajectory: bool = Field(
-        False, 
-        description="Whether or not the current trajectory should be appended to the exitisting traj file"
-    )
-    steps: int = Field(
-        1000,
-        description="The number of steps for which this simulation should run"
-    )
-    save_files: bool = Field(
-        True,
-        description="Whether or not the files from this simulation should be saved"
-    )
-
-
-
-
-
+    ensemble: str = "nvt"
+    temperature: float = 2000.0
+    pressure: float = 1.01325 * units.bar
+    timestep: float = 1.0
+    potential: str = None
+    taut: Optional[float] = None
+    taup: Optional[float] = None
+    compressibility_au: Optional[float] = None
+    trajectory_fn: str = "out.traj"
+    logfile_fn: str = "out.log"
+    loginterval: int = 1
+    append_trajectory: bool = False
+    steps: int = 1000
+    save_files: bool = True
