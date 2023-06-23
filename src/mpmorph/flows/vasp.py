72c19ff8c8460d66fe53ccc4677cfa5c90dadb18
Max Gallant <mcgallant72@gmail.com>
2023-06-23 16:43:28 -0700

    REvert

--- src/mpmorph/flows/vasp.py
+++ src/mpmorph/flows/vasp.py
@@ -36,11 +36,12 @@ def get_md_flow_vasp(
             },
             user_kpoints_settings=my_kpoints
         ),
-        run_vasp_kwargs={
-            "vasp_job_kwargs": {
-                "auto_npar": True
-            }
-        }
+        # This is recommended by VASP, but defaults to false in atomate2...
+        # run_vasp_kwargs={
+        #     "vasp_job_kwargs": {
+        #         "auto_npar": True
+        #     }
+        # }
     )
 
     pv_vasp_maker = MDMaker(
@@ -59,12 +60,7 @@ def get_md_flow_vasp(
                 "LPLANE": False # LPLANE is recommended to be False on Cray machines (https://www.vasp.at/wiki/index.php/LPLANE)
             },
             user_kpoints_settings=my_kpoints
-        ),
-        run_vasp_kwargs={
-            "vasp_job_kwargs": {
-                "auto_npar": True
-            }
-        }
+        )
     )
 
     pv_extractor = PVFromVasp()
